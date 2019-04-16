f7b6fb8f5841b661cd5da575f88db75081e0e1c1
Mr. G <gnmkrtchyan@gmail.com>
2019-04-17 01:26:39 +0300

    feat(): fixed eslint

--- src/__tests__/genDiff.test.js
+++ src/__tests__/genDiff.test.js
@@ -5,6 +5,7 @@ const file1path = 'src/__tests__/__fixtures__/before.json';
 const file2path = 'src/__tests__/__fixtures__/after.json';
 const expectedTest1 = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
 
+/* eslint-disable */
 describe('genDiff tests', () => {
   test('test json', () => {
     expect(genDiff(file1path, file2path)).toBe(expectedTest1);
